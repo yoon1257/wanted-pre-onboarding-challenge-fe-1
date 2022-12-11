3edd738b3dcb7986c8475d28ec0943f83171683d
yuna <yureve5772@gmail.com>
2022-12-11 15:58:20 +0900

    Add: Todo 구현

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./pages/auth/Auth";
+import Todo from "./pages/todo/Todo";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Auth />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
   );
